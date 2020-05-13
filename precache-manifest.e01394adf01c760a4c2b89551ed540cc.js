53a3cb67f275e35c9233bf2f4536ff850a442a73
Olanrewaju Atanda <ooduque@gmail.com>
2020-05-13 23:26:00 +0100

    Updates

---
+++ precache-manifest.e01394adf01c760a4c2b89551ed540cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f843638aea5d054395fd8a4ef7fbf19c",
+    "url": "/covid19-react/index.html"
+  },
+  {
+    "revision": "7d6c4e4272f7a881d08d",
+    "url": "/covid19-react/static/css/main.531f1cc1.chunk.css"
+  },
+  {
+    "revision": "96545858a0bc69c90fcf",
+    "url": "/covid19-react/static/js/2.e71d8e17.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-react/static/js/2.e71d8e17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6c4e4272f7a881d08d",
+    "url": "/covid19-react/static/js/main.8c496e26.chunk.js"
+  },
+  {
+    "revision": "2ee3002896ab160f6a37",
+    "url": "/covid19-react/static/js/runtime-main.e1f76b63.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-react/static/media/image.d7265326.png"
+  }
+]);
